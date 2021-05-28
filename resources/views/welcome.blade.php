0c8967e4ec7d383d36654bab2863eef10e1d8bbc
alnutile <alfrednutile@gmail.com>
2021-05-27 21:34:37 -0400

    removing coming soon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
 <div class="codename-card-wide mdl-card mdl-shadow--2dp" style="margin-top: 10px;">
     <div class="mdl-card__title">
-        <h2 class="mdl-card__title-text">API INFO HERE (COMING SOON)</h2>
+        <h2 class="mdl-card__title-text">API INFO HERE</h2>
     </div>
     <div class="mdl-card__supporting-text">
         <p>All you need to do is make a GET request <b>https://codenames.alfrednutile.info/api/codenames</b></p>
