5687f7ea4b0ef54b3a457b19aab6e6fc4de7f18f
alnutile <alfrednutile@gmail.com>
2021-05-27 21:00:47 -0400

    hmm

--- resources/views/livewire/show-codename.blade.php
+++ resources/views/livewire/show-codename.blade.php
@@ -6,7 +6,10 @@
         <div class="mdl-card__supporting-text">
             Just click the button below to generate a new codename
             <h3>{{ $codename }}</h3>
-            <p class="typography-styles-body2">much thanks to <a target="_blank" href="https://random-data-api.com/documentation">https://random-data-api.com/documentation</a> </p>
+            <p class="typography-styles-body2">man thanks to <a target="_blank" href="https://random-data-api.com/documentation">https://random-data-api.com/documentation</a>
+                <br>
+                and the library `
+            </p>
         </div>
         <div class="mdl-card__actions mdl-card--border">
             <button wire:click="getCodeName" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" wire:loading.attr="disabled" style="min-width: 167.875px;">
