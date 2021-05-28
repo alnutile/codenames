bf1bcee43d4ea559a6e5cc0f771e4d901e2c8c00
alnutile <alfrednutile@gmail.com>
2021-05-27 23:21:44 -0400

    trying the form

--- resources/views/livewire/show-codename.blade.php
+++ resources/views/livewire/show-codename.blade.php
@@ -13,9 +13,11 @@
             </p>
         </div>
         <div class="mdl-card__actions mdl-card--border">
-            <button wire:click="getCodeName" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" wire:loading.attr="disabled" style="min-width: 167.875px;">
-                <span>Generate Another</span>
-            </button>
+            <form wire:submit.prevent="getCodeName">
+                <button wire:click="getCodeName" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" wire:loading.attr="disabled" style="min-width: 167.875px;">
+                    <span>Generate Another</span>
+                </button>
+            </form>
         </div>
     </div>
 </div>
